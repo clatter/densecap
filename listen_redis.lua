0db4fc0e144eaad0fb0216106e5f73e427d9e359
rogowski <michaeljrogowski@gmail.com>
2017-04-20 13:09:28 -0400

    redis listener skel

---
+++ listen_redis.lua
@@ -0,0 +1,10 @@
+
+local cmsgpack = require 'cmsgpack'
+local redis = require 'redis'
+-- TODO: import cmd utils
+local client = redis.connect('127.0.0.1', 6379)
+
+while true do
+  local response = client:blpop('densecap_in', 0)
+  print(response)
+end
